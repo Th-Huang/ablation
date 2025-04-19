b4b3fa16af4dab2b77505d8c2d8a5b0da5d2cd6e
Th_Huang <84441156+Th-Huang@users.noreply.github.com>
2025-04-20 00:17:15 +0800

    Summary

--- ablation_analyze.py
+++ ablation_analyze.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
+import torch
 
 image_path = 'dataset/image/t1'
 data_path = 'dataset/data/t1'
@@ -22,11 +23,7 @@ def read_image(image_path):
             imgs_r.append(img)
     return imgs_l, imgs_r
 
-imgs_l, imgs_r = read_image(image_path)
-print("Left images:", len(imgs_l))
-print("Left images:", imgs_l[0].shape)
-print("Right images:", len(imgs_r))
-print("Right images:", imgs_r[0].shape)
+
 
 def read_data(data_path):
     data_list = os.listdir(data_path)
@@ -45,28 +42,46 @@ def read_data(data_path):
 data, data_strain = read_data(data_path)
 print("Data length:", len(data))
 print("Data strain shape:", data_strain[0].shape)
-imge_ldata = []
-imge_rdata = []
-for i in range(len(data)):
-    file = np.array(data[i])
-    XY = file[:,13:15]
-    img1 = np.array(imgs_l[i])
-    img2 = np.array(imgs_r[i])
-    img_l = []
-    img_r = []
-    for j in range(len(XY)):
-        datal = [int(XY[j][0]),int(XY[j][1]),int(img1[int(XY[j][0]), int(XY[j][1])])]
-        datar = [int(XY[j][0]),int(XY[j][1]),int(img2[int(XY[j][0]), int(XY[j][1])])]
-        img_l.append(datal)
-        img_r.append(datar)
-    img_l = np.array(img_l)
-    img_r = np.array(img_r)
-    imge_ldata.append(img_l)
-    imge_rdata.append(img_r)
 
-imge_rdata = np.array(imge_rdata)
-imge_ldata = np.array(imge_ldata)
-print("Left data length:", len(imge_ldata))
-print("Left data shape:", imge_ldata[0].shape)
-print("Right data length:", len(imge_rdata))
-print("Right data shape:", imge_rdata[0].shape)
+def available_image(imgs_l, imgs_r,data):
+
+    imge_ldata = []
+    imge_rdata = []
+    for i in range(len(data)):
+        file = np.array(data[i])
+        XY = file[:,13:15]
+        img1 = np.array(imgs_l[i])
+        img2 = np.array(imgs_r[i])
+        img_l = []
+        img_r = []
+        for j in range(len(XY)):
+            datal = [int(XY[j][0]),int(XY[j][1]),int(img1[int(XY[j][0]), int(XY[j][1])])]
+            datar = [int(XY[j][0]),int(XY[j][1]),int(img2[int(XY[j][0]), int(XY[j][1])])]
+            img_l.append(datal)
+            img_r.append(datar)
+        img_l = np.array(img_l)
+        img_r = np.array(img_r)
+        imge_ldata.append(img_l)
+        imge_rdata.append(img_r)
+
+    imge_rdata = np.array(imge_rdata)
+    imge_ldata = np.array(imge_ldata)
+
+    return imge_ldata, imge_rdata
+
+def CustomDataset(imge_ldata, imge_rdata, data_strain):
+    # Custom dataset class to handle the data
+    class CustomDataset(torch.utils.data.Dataset):
+        def __init__(self, imge_ldata, imge_rdata, data_strain):
+            self.imge_ldata = imge_ldata
+            self.imge_rdata = imge_rdata
+            self.data_strain = data_strain
+
+        def __len__(self):
+            return len(self.imge_ldata)
+
+        def __getitem__(self, idx):
+            img_l = self.imge_ldata[idx]
+            img_r = self.imge_rdata[idx]
+            strain = self.data_strain[idx]
+            return img_l, img_r, strain
