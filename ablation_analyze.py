dc0918c1f5894f67fb3874e9d717022b4739d91c
Th_Huang <84441156+Th-Huang@users.noreply.github.com>
2025-04-20 17:03:46 +0800

    Summary

--- ablation_analyze.py
+++ ablation_analyze.py
@@ -23,8 +23,6 @@ def read_image(image_path):
             imgs_r.append(img)
     return imgs_l, imgs_r
 
-
-
 def read_data(data_path):
     data_list = os.listdir(data_path)
     data_list.sort(key=lambda x: int(x.replace('t1_', '').split('_')[0]))
@@ -38,36 +36,54 @@ def read_data(data_path):
             data_strain = data[:,6:9]
             datas_strain.append(data_strain)
             all_data.append(data)
+    datas_strain = np.array(datas_strain)
     return all_data,datas_strain
-data, data_strain = read_data(data_path)
-print("Data length:", len(data))
-print("Data strain shape:", data_strain[0].shape)
 
 def available_image(imgs_l, imgs_r,data):
-
     imge_ldata = []
     imge_rdata = []
+    outputstrain = []
     for i in range(len(data)):
         file = np.array(data[i])
         XY = file[:,13:15]
+        X_max = int(np.max(XY[:,0]))
+        X_min = int(np.min(XY[:,0]))
+        Y_max = int(np.max(XY[:,1]))
+        Y_min = int(np.min(XY[:,1]))
+        strain = file[:,6:9]
         img1 = np.array(imgs_l[i])
         img2 = np.array(imgs_r[i])
-        img_l = []
-        img_r = []
+
+        img1 = img1[X_min:X_max+1, Y_min:Y_max+1]
+        img2 = img2[X_min:X_max+1, Y_min:Y_max+1]
+
+        img_l = np.zeros((img1.shape[0], img1.shape[1], 1), dtype=np.uint8)
+        img_r = np.zeros((img2.shape[0], img2.shape[1], 1), dtype=np.uint8)
+        outstrain = np.zeros((img1.shape[0], img1.shape[1], 3), dtype=np.uint8)
         for j in range(len(XY)):
-            datal = [int(XY[j][0]),int(XY[j][1]),int(img1[int(XY[j][0]), int(XY[j][1])])]
-            datar = [int(XY[j][0]),int(XY[j][1]),int(img2[int(XY[j][0]), int(XY[j][1])])]
-            img_l.append(datal)
-            img_r.append(datar)
+            x = int(XY[j][0])
+            y = int(XY[j][1])
+            if x<X_min or x>X_max or y<Y_min or y > Y_max:
+                print("Invalid coordinates:", x, y)
+                continue
+            img_l[x-X_min][y-Y_min]=int(img1[x-X_min,y-Y_min])
+            img_r[x-X_min][y-Y_min]=int(img2[x-X_min,y-Y_min])
+            outstrain[x-X_min][y-Y_min][0]=int(strain[j][0])
+            outstrain[x-X_min][y-Y_min][1]=int(strain[j][1])
+            outstrain[x-X_min][y-Y_min][2]=int(strain[j][2])
         img_l = np.array(img_l)
         img_r = np.array(img_r)
+        outstrain = np.array(outstrain)
+
         imge_ldata.append(img_l)
         imge_rdata.append(img_r)
+        outputstrain.append(outstrain)
 
     imge_rdata = np.array(imge_rdata)
     imge_ldata = np.array(imge_ldata)
+    outputstrain = np.array(outputstrain)
 
-    return imge_ldata, imge_rdata
+    return imge_ldata, imge_rdata,outputstrain
 
 def CustomDataset(imge_ldata, imge_rdata, data_strain):
     # Custom dataset class to handle the data
@@ -81,7 +97,25 @@ def CustomDataset(imge_ldata, imge_rdata, data_strain):
             return len(self.imge_ldata)
 
         def __getitem__(self, idx):
-            img_l = self.imge_ldata[idx]
-            img_r = self.imge_rdata[idx]
-            strain = self.data_strain[idx]
-            return img_l, img_r, strain
+            return (self.imge_ldata[idx], self.imge_rdata[idx], self.data_strain[idx])
+    return CustomDataset(imge_ldata, imge_rdata, data_strain)
+
+
+# imgs_l, imgs_r = read_image(image_path)
+# print("Data length:", len(imgs_l))
+# print("Data strain shape:", imgs_l[0].shape)
+#
+# data, data_strain = read_data(data_path)
+# print("Data length:", len(data))
+# print("Data strain shape:", data_strain[0].shape)
+# data1 = np.array(data)
+# print("Data shape:", data1.shape)
+# X = data1[:,:,13]
+# Y = data1[:,:,14]
+#
+# print('X max',np.max(X))
+# print('X min',np.min(X))
+# print('Y max',np.max(Y))
+# print('Y min',np.min(Y))
+#
+# imge_ldata, imge_rdata, outputstrain = available_image(imgs_l, imgs_r,data)
--- train.py
+++ train.py
@@ -4,26 +4,64 @@ import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
+import torch.utils.data as Data
+
 from unet_model import UNet
 from ablation_analyze import read_data, read_image, available_image , CustomDataset
 
+def executeEpoch(model, dataloader, optimizer, loss_fn, epoch, writer,mode='train'):
+
+    loss_fn = torch.nn.L1Loss(reduction='mean')
+    epoch_loss = 0.0
+    if mode == 'train':
+        model.train()
+    else:
+        model.eval()
+
+    for b, (imge_ldata, imge_rdata, data_strain) in enumerate(dataloader):
+
+        imge_ldata = imge_ldata.cuda()
+        imge_rdata = imge_rdata.cuda()
+        data_strain = data_strain.cuda()
+
+        optimizer.zero_grad()
+        output1 = model(imge_ldata)
+        # output2 = model(imge_rdata)
+        # output = torch.mean(torch.stack([output1, output2]), dim=0)
+        loss = loss_fn(output1, data_strain)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    optimizer.step()
+
+    print(f'Epoch {epoch + 1}/{args.num_epochs}, Loss: {epoch_loss / len(dataloader)}')
+    writer.add_scalar('Loss/train', epoch_loss / len(dataloader), epoch)
+
 def train(args):
+
     imgs_l, imgs_r = read_image(args.image_path)
     data, data_strain = read_data(args.data_path)
-    imge_ldata, imge_rdata = available_image(imgs_l, imgs_r, data)
+    imge_ldata, imge_rdata, out_strain = available_image(imgs_l, imgs_r, data)
 
     imge_ldata = torch.tensor(imge_ldata)
     imge_rdata = torch.tensor(imge_rdata)
-    data_strain = torch.tensor(data_strain)
+    out_strain = torch.tensor(out_strain)
+
+    imge_ldata = imge_ldata.permute(0, 3, 1, 2).float()
+    imge_rdata = imge_rdata.permute(0, 3, 1, 2).float()
+    out_strain = out_strain.permute(0, 3, 1, 2).float()
+
 
     assert all(isinstance(t, torch.Tensor) for t in imge_rdata)
     assert all(isinstance(t, torch.Tensor) for t in imge_ldata)
-    assert all(isinstance(t, torch.Tensor) for t in data_strain)
+    assert all(isinstance(t, torch.Tensor) for t in out_strain)
 
-    dataset = CustomDataset(imge_ldata, imge_rdata, data_strain)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size)
+    dataset = CustomDataset(imge_ldata, imge_rdata, out_strain)
+    dataloader = Data.DataLoader(dataset, batch_size=args.batch_size)
 
-    model = UNet(n_channels=3, n_classes=3).cuda()
+    model = UNet(n_channels=1, n_classes=3).cuda()
     loss_fn = torch.nn.L1Loss(reduction='mean')
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate,eps=1e-8)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
@@ -32,35 +70,15 @@ def train(args):
     writer = SummaryWriter(expPath)
 
     for epoch in range(args.num_epochs):
-        model.train()
-        epoch_loss = 0
-        for batch_idx, (imge_ldata, imge_rdata, data_strain) in enumerate(dataloader):
-            imge_ldata = imge_ldata.cuda()
-            imge_rdata = imge_rdata.cuda()
-            data_strain = data_strain.cuda()
-
-            optimizer.zero_grad()
-            output1 = model(imge_ldata)
-            output2 = model(imge_rdata)
-            output =torch.mean(torch.stack([output1, output2]), dim=0)
-            loss = loss_fn(output, data_strain)
-            loss.backward()
-            optimizer.step()
-
-            epoch_loss += loss.item()
-
-        scheduler.step()
-
-        print(f'Epoch {epoch + 1}/{args.num_epochs}, Loss: {epoch_loss / len(dataloader)}')
-        writer.add_scalar('Loss/train', epoch_loss / len(dataloader), epoch)
-
+        executeEpoch(model, dataloader, optimizer, loss_fn, epoch, writer, mode='train')
     writer.close()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train UNet model')
     parser.add_argument('--image_path', type=str, default='dataset/image/t1', help='Path to the images')
     parser.add_argument('--data_path', type=str, default='dataset/data/t1', help='Path to the data')
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size for training')
     parser.add_argument('--num_epochs', type=int, default=10, help='Number of epochs for training')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for optimizer')
     args = parser.parse_args()
